49d4348308dc93001f8a87f2815e4f8c77f74ea0
Chad Vidovcich <97252914+chadvidovcich@users.noreply.github.com>
2023-02-04 11:06:25 -0800

    fix included font

--- frontend/components/Page.jsx
+++ frontend/components/Page.jsx
@@ -6,8 +6,7 @@ import Header from './Header';
 const GlobalStyles = createGlobalStyle`
 @font-face {
   font-family: 'radnika_next';
-  src: url('/public/static/radnikanext-medium-webfont.woff2')
-  format('woff2');
+  src: url('/static/radnikanext-medium-webfont.woff2') format('woff2');
   font-weight: normal;
   font-style: normal;
 }

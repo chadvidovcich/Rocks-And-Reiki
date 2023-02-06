59801d8bb5866344ed9b62e767d0853184214bf0
Chad Vidovcich <97252914+chadvidovcich@users.noreply.github.com>
2023-02-05 17:15:39 -0800

    delete products, fixes #19

---
+++ frontend/components/DeleteProduct.js
@@ -0,0 +1,36 @@
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/client';
+
+const DELETE_PRODUCT_MUTATION = gql`
+  mutation DELETE_PRODUCT_MUTATION($id: ID!) {
+    deleteProduct(id: $id) {
+      id
+      name
+    }
+  }
+`;
+
+export default function DeleteProduct({ id, children }) {
+  const [deleteProduct, { loading, error }] = useMutation(
+    DELETE_PRODUCT_MUTATION,
+    {
+      variables: {
+        id,
+      },
+    }
+  );
+  return (
+    <button
+      type="button"
+      disabled={loading}
+      onClick={() => {
+        if (confirm('Are you sure you want to delete this item?')) {
+          // ok to delete
+          deleteProduct().catch((err) => alert(err.message));
+        }
+      }}
+    >
+      {children}
+    </button>
+  );
+}

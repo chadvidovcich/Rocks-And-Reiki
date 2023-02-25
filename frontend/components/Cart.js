7ccba75be4e1d2b9cba15d9c10982571aaed3738
Chad Vidovcich <97252914+chadvidovcich@users.noreply.github.com>
2023-02-25 15:01:01 -0800

    cart show/hide state

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -1,4 +1,5 @@
 import calcTotalPrice from '../lib/calcTotalPrice';
+import { useCart } from '../lib/cartState';
 import formatMoney from '../lib/formatMoney';
 import CartItem from './CartItem';
 import CartStyles from './styles/CartStyles';
@@ -7,13 +8,14 @@ import { useUser } from './User';
 
 export default function Cart() {
   const cartUser = useUser();
+  const { cartOpen } = useCart();
   if (!cartUser) {
     return null;
   }
   return (
     <CartStyles
       // @ts-ignore
-      open
+      open={cartOpen}
     >
       <header>
         <Supreme>{cartUser.name}'s Cart</Supreme>

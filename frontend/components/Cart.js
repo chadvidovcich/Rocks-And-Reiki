5af8b76f5aa79dfcfc3abafa030a4d123937df2c
Chad Vidovcich <97252914+chadvidovcich@users.noreply.github.com>
2023-02-25 15:06:43 -0800

    buttons to open/close cart

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -3,12 +3,13 @@ import { useCart } from '../lib/cartState';
 import formatMoney from '../lib/formatMoney';
 import CartItem from './CartItem';
 import CartStyles from './styles/CartStyles';
+import CloseButton from './styles/CloseButton';
 import Supreme from './styles/Supreme';
 import { useUser } from './User';
 
 export default function Cart() {
   const cartUser = useUser();
-  const { cartOpen } = useCart();
+  const { cartOpen, closeCart } = useCart();
   if (!cartUser) {
     return null;
   }
@@ -19,6 +20,9 @@ export default function Cart() {
     >
       <header>
         <Supreme>{cartUser.name}'s Cart</Supreme>
+        <CloseButton type="button" onClick={closeCart}>
+          &times;
+        </CloseButton>
       </header>
       <ul>
         {cartUser.cart.map((cartItem) => (

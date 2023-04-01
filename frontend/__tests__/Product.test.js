328cb7f5082f39afbf224bc0f2fda087abd980a1
Chad Vidovcich <97252914+chadvidovcich@users.noreply.github.com>
2023-04-01 10:49:02 -0700

    product testing

--- frontend/__tests__/Product.test.js
+++ frontend/__tests__/Product.test.js
@@ -20,4 +20,23 @@ describe('<Product/>', () => {
     expect(link).toHaveAttribute('href', `/product/${product.id}`);
     expect(link).toHaveTextContent(product.name);
   });
+
+  it('Renders and matches the snapshot', () => {
+    const { container, debug } = render(
+      <MockedProvider>
+        <Product product={product} />
+      </MockedProvider>
+    );
+    expect(container).toMatchSnapshot();
+  });
+
+  it('Renders the image properly', () => {
+    const { container, debug } = render(
+      <MockedProvider>
+        <Product product={product} />
+      </MockedProvider>
+    );
+    const img = screen.getByAltText(product.name);
+    expect(img).toBeInTheDocument();
+  });
 });

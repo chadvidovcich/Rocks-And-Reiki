dec560a4a81cd309ebea5070db1902fad8acb75e
Chad Vidovcich <97252914+chadvidovcich@users.noreply.github.com>
2023-04-01 11:51:07 -0700

    remove capital in test title

--- frontend/__tests__/formatMoney.test.js
+++ frontend/__tests__/formatMoney.test.js
@@ -1,6 +1,6 @@
 import formatMoney from '../lib/formatMoney';
 
-describe('format Money function', () => {
+describe('format money function', () => {
   it('works with fractional dollars', () => {
     expect(formatMoney(1)).toEqual('$0.01');
     expect(formatMoney(5)).toEqual('$0.05');
